7b67ccd08d56b1007f4baf3b567bf9e995913d47
Vladimir Victor Pavlychev <vladimir.keyjoo@gmail.com>
2023-07-25 20:01:44 +0300

    Add: Interface of App(screen)

--- lib/screen/magic_ball_screen.dart
+++ lib/screen/magic_ball_screen.dart
@@ -5,6 +5,52 @@ class MagicBallScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            //Decoration
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.black87,
+                    Color.fromARGB(255, 36, 6, 85),
+                  ],
+                ),
+              ),
+            ),
+
+
+            
+            //Image
+            Center(
+              child: Image.asset('assets/images/ball.png'),
+            ),
+
+            //Text
+            Positioned(
+              bottom: 30,
+              
+              child: RichText(
+                text: const TextSpan(
+                  text: ' Нажмите на шар или \n потрясите телефон',
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 16,
+                    color: Colors.amber),
+                ),
+              ),
+            ),
+        
+
+
+          ],
+        ),
+      ),
+    );
   }
 }
